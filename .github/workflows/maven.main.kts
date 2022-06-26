3f9dea5d5981e549cd5dd5b131672d6f55ddee66
Uwe Schaefer <uwe.schaefer@prisma-capacity.eu>
2022-06-26 15:23:23 +0200

    use-kotlin-dsl-for-ghactions

--- .github/workflows/maven.main.kts
+++ .github/workflows/maven.main.kts
@@ -1,6 +1,6 @@
 #!/usr/bin/env kotlin
 
-@file:DependsOn("it.krzeminski:github-actions-kotlin-dsl:0.20.0")
+@file:DependsOn("it.krzeminski:github-actions-kotlin-dsl:0.19.0")
 
 import it.krzeminski.githubactions.actions.actions.CheckoutV3
 import it.krzeminski.githubactions.actions.actions.SetupJavaV3
