6d11e87470f649b30a0f42e539068329806c00d7
Uwe Schaefer <uwe.schaefer@prisma-capacity.eu>
2022-06-26 15:21:01 +0200

    use-kotlin-dsl-for-ghactions

--- .github/workflows/maven.main.kts
+++ .github/workflows/maven.main.kts
@@ -1,47 +1,44 @@
 #!/usr/bin/env kotlin
 
-@file:DependsOn("it.krzeminski:github-actions-kotlin-dsl:0.19.0")
+@file:DependsOn("it.krzeminski:github-actions-kotlin-dsl:0.20.0")
 
-import it.krzeminski.githubactions.actions.actions.CheckoutV2
 import it.krzeminski.githubactions.actions.actions.CheckoutV3
 import it.krzeminski.githubactions.actions.actions.SetupJavaV3
 import it.krzeminski.githubactions.domain.RunnerType
 import it.krzeminski.githubactions.domain.Workflow
 import it.krzeminski.githubactions.domain.triggers.Push
-import it.krzeminski.githubactions.dsl.expr
 import it.krzeminski.githubactions.dsl.workflow
-import it.krzeminski.githubactions.yaml.toYaml
 import it.krzeminski.githubactions.yaml.writeToFile
-import java.nio.`file`.Paths
+import java.nio.file.Paths
 
 public val workflowMaven: Workflow = workflow(
-      name = "Java CI",
-      on = listOf(
+    name = "Java CI",
+    on = listOf(
         Push(),
-        ),
-      sourceFile = Paths.get("maven.main.kts"),
-    ) {
-      job(
+    ),
+    sourceFile = Paths.get("maven.main.kts"),
+) {
+    job(
         id = "build",
         runsOn = RunnerType.UbuntuLatest,
-      ) {
+    ) {
         uses(
-          name = "Checkout",
-          action = CheckoutV3(),
+            name = "Checkout",
+            action = CheckoutV3(),
         )
         uses(
-          name = "Set up JDK 1.8",
-          action = SetupJavaV3(
-            javaVersion = "1.8",
-            distribution = SetupJavaV3.Distribution.Corretto
-          ),
+            name = "Set up JDK 1.8",
+            action = SetupJavaV3(
+                javaVersion = "1.8",
+                distribution = SetupJavaV3.Distribution.Corretto
+            ),
         )
         run(
-          name = "Build with Maven",
-          command = "mvn -B package --file pom.xml",
+            name = "Build with Maven",
+            command = "mvn -B package --file pom.xml",
         )
-      }
-
     }
+
+}
 
 workflowMaven.writeToFile()
